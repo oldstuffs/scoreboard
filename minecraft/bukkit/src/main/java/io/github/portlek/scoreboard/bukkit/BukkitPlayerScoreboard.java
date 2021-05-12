ff9ae132244d2ab44d4627b8c437c17fc514c0be
Hasan Demirtaş <utsukushihito@outlook.com>
2021-05-12 21:01:21 +0300

    Update BukkitPlayerScoreboard.java

--- minecraft/bukkit/src/main/java/io/github/portlek/scoreboard/bukkit/BukkitPlayerScoreboard.java
+++ minecraft/bukkit/src/main/java/io/github/portlek/scoreboard/bukkit/BukkitPlayerScoreboard.java
@@ -315,11 +315,8 @@ public final class BukkitPlayerScoreboard implements Closeable {
 
   /**
    * setups the player.
-   *
-   * @return {@code this} for builder chain.
    */
-  @NotNull
-  private BukkitPlayerScoreboard setup() {
+  private void setup() {
     Bukkit.getScheduler().runTask(this.plugin, () -> {
       final var player = Bukkit.getPlayer(this.uniqueId);
       if (player != null) {
@@ -328,7 +325,6 @@ public final class BukkitPlayerScoreboard implements Closeable {
       }
       this.setup.set(true);
     });
-    return this;
   }
 
   /**
